55654da29ee3f934b252b6854a1197b168a2ac67
wzc-OntarioTechU <william.chamberlain@ontariotechu.net>
2025-03-14 09:14:18 +0800

    Created ROS catkin workspace and an empty package.

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/wzc/ros-deployment/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/wzc/ros-deployment/devel;/home/jetson/software/laser_app/devel;/home/jetson/software/rtabmap_ws/devel;/home/jetson/software/carto_ws/devel_isolated/cartographer_rviz;/home/jetson/software/carto_ws/install_isolated;/home/jetson/software/library_ws/devel;/home/jetson/software/ar_track_ws/devel;/home/jetson/yahboomcar_ws/devel;/opt/ros/noetic'.split(';') if '/home/wzc/ros-deployment/devel;/home/jetson/software/laser_app/devel;/home/jetson/software/rtabmap_ws/devel;/home/jetson/software/carto_ws/devel_isolated/cartographer_rviz;/home/jetson/software/carto_ws/install_isolated;/home/jetson/software/library_ws/devel;/home/jetson/software/ar_track_ws/devel;/home/jetson/yahboomcar_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/jetson/software/laser_app/devel/lib:/home/jetson/software/rtabmap_ws/devel/lib:/home/jetson/software/carto_ws/devel_isolated/cartographer_rviz/lib:/home/jetson/software/carto_ws/install_isolated/lib:/home/jetson/software/library_ws/devel/lib:/home/jetson/software/ar_track_ws/devel/lib:/home/jetson/yahboomcar_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/aarch64-linux-gnu:/usr/local/cuda/lib64:/home/jetson/software/TensorRT-8.6.0.12.Linux.x86_64-gnu.cuda-11.8/TensorRT-8.6.0.12/lib'
+export PWD='/home/wzc/ros-deployment/build'
